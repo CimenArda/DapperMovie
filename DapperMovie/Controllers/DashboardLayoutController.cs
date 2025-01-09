378f266cd201d010f3e55e7aab1363a93484412a
ArdaCimen <cimenarda.yaz@gmail.com>
2025-01-09 13:53:04 +0300

    Add project files.

---
+++ DapperMovie/Controllers/DashboardLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DapperMovie.Controllers
+{
+    public class DashboardLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
